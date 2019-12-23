5f5e7bd3e2acf1df96b82b42677954f59bc5a9e6
Mohib <mbmohib@gmail.com>
2019-12-23 17:48:30 +0600

    add model

---
+++ src/models/Todo.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const Todo = mongoose.model('Todo', {
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+module.exports = Todo;
